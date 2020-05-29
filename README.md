f3cfd9856f508fa3976f9e5b33c7c914ab2bdb19
Pierre De Schoolmeester <pierre.deschoolmeester.contractor@dvla.gov.uk>
2020-05-29 23:42:50 +0100

    initial commit :)

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# serverless-s3-delete
-Serverless plugin to empty buckets including versioned buckets
+# serverless-s3-bucket-clearer
+Serverless plugin to empty buckets and versioned buckets. 
+
+# Usage
+```bash
+$ npm install serverless-s3-bucket-clearer
+```
+
+Add the following to your serverless yml template. 
+```yaml
+
+plugins:
+    - serverless-s3-bucket-clearer
+
+custom:
+    s3Clearer:
+        enabled: false # default enabled
+        buckets: #list of buckets to be emptied
+            - your-bucket-1  
+            - your-bucket-2
+```
+
+### Removing a stack with the plugin enabled.
+
+You can empty out the buckets with
+```bash
+$ sls s3clear
+```
+
+On removing a serverless stack the plugin will empty out buckets listed allowing for the buckets to be removed.
+
+```bash
+$ sls remove 
+```
+
---
+++ index.js
@@ -0,0 +1,81 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+const checkBucketExists = ( Bucket ) => new Promise((resolve, reject) => {
+    s3.headBucket({ Bucket}, (err) => {
+        if (err) {
+            if( err.statusCode === 404) {
+                resolve(false)  
+            }
+            reject(err);
+        }
+        resolve(true)
+    });
+})
+const getBucketObjects = ( Bucket) => new Promise((resolve, reject) => {
+    s3.listObjectVersions({Bucket}, (err, data) => {
+        if (err) {
+            reject(err)
+        } 
+        const versions = data.Versions.map(obj => ({ Key: obj.Key, VersionId: obj.VersionId}));
+        const deleteMarkers = data.DeleteMarkers.map(obj => ({ Key: obj.Key, VersionId: obj.VersionId}))
+        resolve(versions.concat(deleteMarkers)); 
+    });  
+});
+const removeAllObjects = (bucketObjects, bucket) => new Promise((resolve, reject) => {
+    s3.deleteObjects({ Bucket: bucket, Delete: {Objects: bucketObjects}}, (err, data) => {
+        if (err) {
+            reject(err);
+        }
+        resolve(data)
+    })
+})
+
+const clearBuckets = async (serverless, options) => {
+    
+    const { s3clear: s3ClearOptions } = serverless.service.custom;
+    if( s3ClearOptions.enabled === false) {
+        serverless.cli.log("Skipping serverless-s3-clearer as enabled set to false");
+        return;
+    }
+    if(s3ClearOptions && s3ClearOptions.buckets) {
+        for (const bucket of s3ClearOptions.buckets) {
+            try{
+                if(await checkBucketExists(bucket)) {
+                    serverless.cli.log(`${bucket} exists`);
+                    const bucketObjects =  await getBucketObjects(bucket);
+                    if (bucketObjects.length) {
+                        await removeAllObjects(bucketObjects, bucket);
+                    }
+                    serverless.cli.log(`${bucket} empty`)
+                
+                } else {
+                    serverless.cli.log(bucket, 'does not exist');
+                };
+            }catch (err){
+                console.log('ERROR', err)
+            }
+        }
+    }
+}
+
+class s3ClearerPlugin {
+    constructor(serverless, options) {
+        this.serverless = serverless;
+        this.options = options;
+        this.commands = {
+            s3clear: {
+                lifecycleEvents: [
+                    'remove',
+                ],
+            }
+        };
+        this.hooks = {
+            'before:remove:remove': clearBuckets.bind(null, serverless, options),
+            's3clear:remove': clearBuckets.bind(null, serverless, options),
+        };
+    }
+}
+
+module.exports = s3ClearerPlugin;
+
