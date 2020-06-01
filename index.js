2722a97b6c20d2e8051af74f2f08dbf9edc4a028
Pierre De Schoolmeester <pierre.deschoolmeester.contractor@dvla.gov.uk>
2020-06-01 16:19:35 +0100

    update to recursivley call deletion if list is truncated

--- index.js
+++ index.js
@@ -1,63 +1,74 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
-const checkBucketExists = ( Bucket ) => new Promise((resolve, reject) => {
-    s3.headBucket({ Bucket}, (err) => {
-        if (err) {
-            if( err.statusCode === 404) {
-                resolve(false)  
+const checkBucketExists = (Bucket) =>
+    new Promise((resolve, reject) => {
+        s3.headBucket({ Bucket }, (err) => {
+            if (err) {
+                if (err.statusCode === 404) {
+                    resolve(false);
+                }
+                reject(err);
             }
-            reject(err);
-        }
-        resolve(true)
+            resolve(true);
+        });
     });
-})
-const getBucketObjects = ( Bucket) => new Promise((resolve, reject) => {
-    s3.listObjectVersions({Bucket}, (err, data) => {
-        if (err) {
-            reject(err)
-        } 
-        const versions = data.Versions.map(obj => ({ Key: obj.Key, VersionId: obj.VersionId}));
-        const deleteMarkers = data.DeleteMarkers.map(obj => ({ Key: obj.Key, VersionId: obj.VersionId}))
-        resolve(versions.concat(deleteMarkers)); 
-    });  
-});
-const removeAllObjects = (bucketObjects, bucket) => new Promise((resolve, reject) => {
-    s3.deleteObjects({ Bucket: bucket, Delete: {Objects: bucketObjects}}, (err, data) => {
-        if (err) {
-            reject(err);
-        }
-        resolve(data)
-    })
-})
+const getBucketObjects = (Bucket) =>
+    new Promise((resolve, reject) => {
+        s3.listObjectVersions({ Bucket }, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            const { IsTruncated } = data;
+            const versions = data.Versions.map((obj) => ({ Key: obj.Key, VersionId: obj.VersionId }));
+            const deleteMarkers = data.DeleteMarkers.map((obj) => ({ Key: obj.Key, VersionId: obj.VersionId }));
+            resolve({ IsTruncated, bucketObjects: versions.concat(deleteMarkers) });
+        });
+    });
+
+const deleteBucketObjects = (bucket, bucketObjects) =>
+    new Promise((resolve, reject) => {
+        s3.deleteObjects({ Bucket: bucket, Delete: { Objects: bucketObjects } }, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(data);
+        });
+    });
+
+const removeAllObjects = async (bucket, serverless) => {
+    const { IsTruncated, bucketObjects } = await getBucketObjects(bucket, serverless);
+    await deleteBucketObjects(bucket, bucketObjects);
+    if (IsTruncated) {
+        serverless.cli.log(`${bucket} isTruncated doing recursive call`);
+        await removeAllObjects(bucket, serverless);
+    } else {
+        return (`${bucket} emptied`);
+    }
+};
+
 
 const clearBuckets = async (serverless, options) => {
-    
     const { s3clear: s3ClearOptions } = serverless.service.custom;
-    if( s3ClearOptions.enabled === false) {
-        serverless.cli.log("Skipping serverless-s3-clearer as enabled set to false");
+    if (s3ClearOptions.enabled === false) {
+        serverless.cli.log('Skipping serverless-s3-clearer as enabled set to false');
         return;
     }
-    if(s3ClearOptions && s3ClearOptions.buckets) {
+    if (s3ClearOptions && s3ClearOptions.buckets) {
         for (const bucket of s3ClearOptions.buckets) {
-            try{
-                if(await checkBucketExists(bucket)) {
-                    serverless.cli.log(`${bucket} exists`);
-                    const bucketObjects =  await getBucketObjects(bucket);
-                    if (bucketObjects.length) {
-                        await removeAllObjects(bucketObjects, bucket);
-                    }
-                    serverless.cli.log(`${bucket} empty`)
-                
+            try {
+                if (await checkBucketExists(bucket)) {
+                    const result = await removeAllObjects(bucket, serverless);
+                    serverless.cli.log(`${result}`);
                 } else {
                     serverless.cli.log(bucket, 'does not exist');
-                };
-            }catch (err){
-                console.log('ERROR', err)
+                }
+            } catch (err) {
+                console.log('ERROR', err);
             }
         }
     }
-}
+};
 
 class s3ClearerPlugin {
     constructor(serverless, options) {
@@ -65,10 +76,8 @@ class s3ClearerPlugin {
         this.options = options;
         this.commands = {
             s3clear: {
-                lifecycleEvents: [
-                    'remove',
-                ],
-            }
+                lifecycleEvents: ['remove'],
+            },
         };
         this.hooks = {
             'before:remove:remove': clearBuckets.bind(null, serverless, options),
@@ -78,4 +87,3 @@ class s3ClearerPlugin {
 }
 
 module.exports = s3ClearerPlugin;
-
